ab3a199f5576c7012bf4fd8884f44c31b4e943f8
firemasterleo <firemasterleo@outlook.com>
2025-01-25 07:49:02 -0500

    commit

--- src/components/Section4.vue
+++ src/components/Section4.vue
@@ -1,180 +1,333 @@
 <template>
     <div class="sectioncontainer">
-      <div class="section">
-        <div class="work-header">
+        <div class="section">
+            <div class="work-header">
                 <h1>WORK</h1>
+            </div>
+            <div class="works">
+                <div class="works-child">
+                    <div class="image">
+                        <img src="../assets/vom-image1.jpeg" loading="lazy" alt="">
+                    </div>
+                </div>
+                <div class="works-child">
+                    <div class="image">
+                        <img src="../assets/vom-image6.jpeg" loading="lazy" alt="">
+                    </div>
+                </div>
+                <div class="works-child">
+                    <div class="image">
+                        <img src="../assets/vom-image3.jpeg" loading="lazy" alt="">
+                    </div>
+                </div>
+                <div class="works-child">
+                    <div class="image">
+                        <img src="../assets/vom-image4.jpeg" loading="lazy" alt="">
+                    </div>
+                </div>
+            </div>
+            <div class="infinite-scroll">
+                <div class="inner-scroller">
+                    <div>
+                        <p>Ready to connect ?</p>
+                    </div>
+                    <div class="svg">
+                        <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='#000000' width='24' height='24'>
+                            <path d="M19.649 5.286 14 8.548V2.025h-4v6.523L4.351 5.286l-2 3.465 5.648 3.261-5.648 3.261 2 3.465L10 15.477V22h4v-6.523l5.649 3.261 2-3.465-5.648-3.261 5.648-3.261z"></path>
+                        </svg>
+                    </div>
+                    <div>
+                        <p>Say Hello.</p>
+                    </div>
+                    <div class="svg">
+                        <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='#000000' width='24' height='24'>
+                            <path d="M19.649 5.286 14 8.548V2.025h-4v6.523L4.351 5.286l-2 3.465 5.648 3.261-5.648 3.261 2 3.465L10 15.477V22h4v-6.523l5.649 3.261 2-3.465-5.648-3.261 5.648-3.261z"></path>
+                        </svg>
+                    </div>
+                </div>
+                <div class="inner-scroller">
+                    <div>
+                        <p>Ready to connect ?</p>
+                    </div>
+                    <div class="svg">
+                        <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='#000000' width='24' height='24'>
+                            <path d="M19.649 5.286 14 8.548V2.025h-4v6.523L4.351 5.286l-2 3.465 5.648 3.261-5.648 3.261 2 3.465L10 15.477V22h4v-6.523l5.649 3.261 2-3.465-5.648-3.261 5.648-3.261z"></path>
+                        </svg>
+                    </div>
+                    <div>
+                        <p>Say Hello.</p>
+                    </div>
+                    <div class="svg">
+                        <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='#000000' width='24' height='24'>
+                            <path d="M19.649 5.286 14 8.548V2.025h-4v6.523L4.351 5.286l-2 3.465 5.648 3.261-5.648 3.261 2 3.465L10 15.477V22h4v-6.523l5.649 3.261 2-3.465-5.648-3.261 5.648-3.261z"></path>
+                        </svg>
+                    </div>
+                </div>
+                <div class="inner-scroller">
+                    <div>
+                        <p>Ready to connect ?</p>
+                    </div>
+                    <div class="svg">
+                        <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='#000000' width='24' height='24'>
+                            <path d="M19.649 5.286 14 8.548V2.025h-4v6.523L4.351 5.286l-2 3.465 5.648 3.261-5.648 3.261 2 3.465L10 15.477V22h4v-6.523l5.649 3.261 2-3.465-5.648-3.261 5.648-3.261z"></path>
+                        </svg>
+                    </div>
+                    <div>
+                        <p>Say Hello.</p>
+                    </div>
+                    <div class="svg">
+                        <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='#000000' width='24' height='24'>
+                            <path d="M19.649 5.286 14 8.548V2.025h-4v6.523L4.351 5.286l-2 3.465 5.648 3.261-5.648 3.261 2 3.465L10 15.477V22h4v-6.523l5.649 3.261 2-3.465-5.648-3.261 5.648-3.261z"></path>
+                        </svg>
+                    </div>
+                </div>
+                <div class="inner-scroller">
+                    <div>
+                        <p>Ready to connect ?</p>
+                    </div>
+                    <div class="svg">
+                        <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='#000000' width='24' height='24'>
+                            <path d="M19.649 5.286 14 8.548V2.025h-4v6.523L4.351 5.286l-2 3.465 5.648 3.261-5.648 3.261 2 3.465L10 15.477V22h4v-6.523l5.649 3.261 2-3.465-5.648-3.261 5.648-3.261z"></path>
+                        </svg>
+                    </div>
+                    <div>
+                        <p>Say Hello.</p>
+                    </div>
+                    <div class="svg">
+                        <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='#000000' width='24' height='24'>
+                            <path d="M19.649 5.286 14 8.548V2.025h-4v6.523L4.351 5.286l-2 3.465 5.648 3.261-5.648 3.261 2 3.465L10 15.477V22h4v-6.523l5.649 3.261 2-3.465-5.648-3.261 5.648-3.261z"></path>
+                        </svg>
+                    </div>
+                </div>
+                <div class="inner-scroller">
+                    <div>
+                        <p>Ready to connect ?</p>
+                    </div>
+                    <div class="svg">
+                        <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='#000000' width='24' height='24'>
+                            <path d="M19.649 5.286 14 8.548V2.025h-4v6.523L4.351 5.286l-2 3.465 5.648 3.261-5.648 3.261 2 3.465L10 15.477V22h4v-6.523l5.649 3.261 2-3.465-5.648-3.261 5.648-3.261z"></path>
+                        </svg>
+                    </div>
+                    <div>
+                        <p>Say Hello.</p>
+                    </div>
+                    <div class="svg">
+                        <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='#000000' width='24' height='24'>
+                            <path d="M19.649 5.286 14 8.548V2.025h-4v6.523L4.351 5.286l-2 3.465 5.648 3.261-5.648 3.261 2 3.465L10 15.477V22h4v-6.523l5.649 3.261 2-3.465-5.648-3.261 5.648-3.261z"></path>
+                        </svg>
+                    </div>
+                </div>
+                <div class="inner-scroller">
+                    <div>
+                        <p>Ready to connect ?</p>
+                    </div>
+                    <div class="svg">
+                        <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='#000000' width='24' height='24'>
+                            <path d="M19.649 5.286 14 8.548V2.025h-4v6.523L4.351 5.286l-2 3.465 5.648 3.261-5.648 3.261 2 3.465L10 15.477V22h4v-6.523l5.649 3.261 2-3.465-5.648-3.261 5.648-3.261z"></path>
+                        </svg>
+                    </div>
+                    <div>
+                        <p>Say Hello.</p>
+                    </div>
+                    <div class="svg">
+                        <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='#000000' width='24' height='24'>
+                            <path d="M19.649 5.286 14 8.548V2.025h-4v6.523L4.351 5.286l-2 3.465 5.648 3.261-5.648 3.261 2 3.465L10 15.477V22h4v-6.523l5.649 3.261 2-3.465-5.648-3.261 5.648-3.261z"></path>
+                        </svg>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="works">
-            <div class="works-child">
-                <div class="image">
-                    <img src="../assets/vom-image1.jpeg" loading="lazy" alt="">
-                </div>
-            </div>
-
-            <div class="works-child">
-                <div class="image">
-                    <img src="../assets/vom-image6.jpeg" loading="lazy" alt="">
-                </div>
-            </div>
-
-            <div class="works-child">
-                <div class="image">
-                    <img src="../assets/vom-image3.jpeg" loading="lazy" alt="">
-
-                </div>
-            </div>
-
-            <div class="works-child">
-                                <div class="image">
-                                    <img src="../assets/vom-image4.jpeg" loading="lazy" alt="">
-
-                </div>
-            </div>
-
-
-        </div>
-        <div class="infinite-scroll">
-
-        </div>
-
-
-  
-      </div>
     </div>
-  </template>
-  
+</template>
 
-<script script setup>
-
+<script setup>
 </script>
 
 <style lang="scss" scoped>
- @import '../sass/variables';
+@import '../sass/variables';
+
 .sectioncontainer {
     background-color: #111315;
     width: 100%;
-  height: fit-content; /* Large enough to allow scrolling */
-display: flex;
+    height: fit-content; /* Large enough to allow scrolling */
+    display: flex;
 
+    .section {
+        background-color: $bgcolorwhite; /* Solid black background */
+        width: 80rem;
+        height: 100%; /* Full viewport height */
+        margin-inline: auto;
 
-  .section {
-  background-color: $bgcolorwhite; /* Solid black background */
-    width: 80rem;
-    height: 100%; /* Full viewport height */
-    margin-inline: auto;
+        .work-header {
+            height: 100vh;
+            background-color: white;
+            // border: solid red;
+            position: sticky; /* Sticky positioning */
+            top: 0;
+            color: $bgcolorblack;
 
-    .work-header {
-        height: 100vh;
-        background-color: white;
-        // border: solid red;
-
-        position: sticky; /* Sticky positioning */
-        top: 0;
-        color: $bgcolorblack;
-
-        h1 {
-            margin: 0;
-      font-size: calc(25vw + 10vh); /* Dynamically adjust font size */
-      text-align: center;
-      color: #333;
-      width: 100%;
-        }
-    }
-    .works {
-        height: fit-content;
-        // border: solid red;
-        display: flex;
-        flex-direction: column;
-        gap: 1rem;
-
-        .works-child {
-            // height: 24rem;
-            // border: solid blue;
-            background-color: $bgcolorblack;
-
-            .image {
+            h1 {
+                margin: 0;
+                font-size: calc(25vw + 10vh); /* Dynamically adjust font size */
+                text-align: center;
+                color: #333;
                 width: 100%;
-                height: 100%;
+            }
+        }
 
-                img {
+        .works {
+            height: fit-content;
+            // border: solid red;
+            display: flex;
+            flex-direction: column;
+            gap: 1rem;
+
+            .works-child {
+                // height: 24rem;
+                // border: solid blue;
+                background-color: $bgcolorblack;
+
+                .image {
                     width: 100%;
                     height: 100%;
-                    object-fit: cover;
+
+                    img {
+                        width: 100%;
+                        height: 100%;
+                        object-fit: cover;
+                    }
                 }
-
             }
-
         }
 
-    }
-    .infinite-scroll {
-        height: 7rem;
+        .infinite-scroll {
+            height: 7rem;
+            width: 100%;
+            // border: solid blue;
+            border-radius: 0 0 2rem 2rem;
+            background-color: $bgcolorwhite;
+            // white-space: nowrap;
+            display: flex;
+            gap: 1.3rem;
+            overflow: hidden;
+            align-items: center;
 
-        // border: solid blue;
-        border-radius: 0 0 2rem 2rem;
-        background-color: $bgcolorwhite;
-    
+            .inner-scroller {
+                // width: 50rem;
+                color: $textcolorblack;
+                // margin-inline: 2rem;
+                display: flex;
+                gap: 1rem;
+                animation: scrollAnimation 5s linear infinite;
+
+                p {
+                    // margin-inline: 40px;
+                    white-space: nowrap;
+                }
+
+                .svg {
+                    min-width: 1rem;
+                }
+            }
+        }
     }
-  }
 }
-
-
 
 @media (max-width: 800px) {
-  .sectioncontainer {
-    .section {
-      width: 100vw;
-      border-radius: 0 0 1rem 1rem;
+    .sectioncontainer {
+        .section {
+            width: 100vw;
+            border-radius: 0 0 1rem 1rem;
 
+            .work-header {
+                height: 20rem;
+                background-color: $bgcolorwhite;
+                position: relative;
+                display: flex;
+                justify-content: center;
+                align-items: center;
 
-      .work-header {
-        height: 20rem;
-        background-color: $bgcolorwhite;
-        position: relative; 
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        
-        
-        h1 {
-            margin: 0;
-      font-size: calc(10vw + 10vh); /* Dynamically adjust font size */
-      text-align: center;
-      color: #333;
-      width: 100%;
-        }
-        
-    }
-    .works {
-        padding-inline: 0.5rem;
-        .works-child {
-            height: 21rem;
-            width: 100%;
-            border: none;
-
-            .image {
-                width: 100%;
-                height: 100%;
-
-                img {
+                h1 {
+                    margin: 0;
+                    font-size: calc(10vw + 10vh); /* Dynamically adjust font size */
+                    text-align: center;
+                    color: #333;
                     width: 100%;
-                    height: 100%;
-                    object-fit: cover;
                 }
-
             }
 
+            .works {
+                padding-inline: 0.5rem;
 
+                .works-child {
+                    height: 21rem;
+                    width: 100%;
+                    border: none;
+
+                    .image {
+                        width: 100%;
+                        height: 100%;
+
+                        img {
+                            width: 100%;
+                            height: 100%;
+                            object-fit: cover;
+                        }
+                    }
+                }
+            }
+            .infinite-scroll {
+            height: 7rem;
+            width: 100%;
+            // border: solid blue;
+            border-radius: 0 0 2rem 2rem;
+            gap: 1.3rem;
+    
+
+            .inner-scroller {
+                // border: solid red;
+                // width: 50rem;
+                color: $textcolorblack;
+                // margin-inline: 2rem;
+                display: flex;
+                gap: 1rem;
+                animation: scrollAnimation 5s linear infinite;
+
+                div {
+
+                    p {
+                        // margin-inline: 40px;
+                        white-space: nowrap;
+                        font-size: 22px;
+                        color: $textcolorblackdeepest;
+                    }
+                }
+
+
+                .svg {
+                    min-width: 1rem;
+                    display: flex;
+                    align-items: center;
+                }
+            }
+        }
         }
     }
-    }
-  }
 }
-@media (max-width: 1200px) and (min-width: 800px) {
-  .sectioncontainer {
-    .section {
 
+@media (max-width: 1200px) and (min-width: 800px) {
+    .sectioncontainer {
+        .section {
+        }
     }
-  }
+}
+
+@keyframes scrollAnimation {
+    from {
+        transform: translateX(0);
+    }
+    to {
+        transform: translateX(-105%);
+    }
 }
 </style>
