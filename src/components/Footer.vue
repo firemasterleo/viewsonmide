4d1c193f8ef42c5f62920eadf0887b79cade17e1
firemasterleo <firemasterleo@outlook.com>
2025-01-25 02:35:04 -0500

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -208,9 +208,9 @@ onMounted(() => {
 
     gsap.fromTo(
   footer.value,
-  { y: '-20%' }, // Starting value
+  { y: '-30%' }, // Starting value
   {
-    y: '20%', // Ending value
+    y: '30%', // Ending value
     ease: 'none',
     scrollTrigger: {
       trigger: footer.value,
