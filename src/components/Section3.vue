364c27f62d13c735d9c2614daeedf31675fa653c
firemasterleo <firemasterleo@outlook.com>
2025-01-26 09:37:58 -0500

    Update Section3.vue

--- src/components/Section3.vue
+++ src/components/Section3.vue
@@ -50,7 +50,7 @@
       </div>
       <div class="video-container">
         <video autoplay muted loop playsinline>
-          <source src="../assets/vom-video3.mp4" type="video/mp4">
+          <source src="https://res.cloudinary.com/dgdnews6i/video/upload/v1737901329/vom-video2_1_uyjy7d.mp4" type="video/mp4">
           Your browser does not support the video tag.
         </video>
       </div>
