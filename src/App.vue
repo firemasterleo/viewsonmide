7d201ff7643d5117a44adc737611de8412fbc4b4
firemasterleo <firemasterleo@outlook.com>
2025-02-01 04:50:12 -0500

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 
 <template>
+    <div v-if="showOverlay" class="overlay">
+    <h1>Loading...</h1> <!-- Or an animation -->
+  </div>
   <div class="routersection">
     <router-view/>
   </div>
@@ -9,7 +12,20 @@
 @use './sass/main.scss';
 
 
-
+.overlay {
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  background: black;
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 2rem;
+  z-index: 9999;
+  opacity: 1;
+  transition: opacity 0.5s ease-out;
+}
 
 
 </style>
@@ -20,8 +36,18 @@ import { onMounted, onBeforeUnmount } from 'vue';
 
 
 import LenisScroll from './components/LenisScroll.vue';
-
-
+import { ref } from "vue";
+const showOverlay = ref(false);
 const router = useRouter();
+
+router.beforeEach((to, from, next) => {
+  showOverlay.value = true; // Show transition screen
+
+  setTimeout(() => {
+    showOverlay.value = false; // Hide after 1s
+    next(); // Navigate to new route
+  }, 1000);
+});
+
 
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -26,24 +26,7 @@ const router = createRouter({
         },
         
     ],
-    scrollBehavior(to, from, savedPosition) {
-      if (savedPosition) {
-        // When coming back from another route, restore the scroll position
-        return savedPosition;
-      } else {
-        // Otherwise, scroll to top (or another position as needed)
-        return { x: 0, y: 0 };
-      }
-    },
   
 });
-window.addEventListener('scroll', () => {
-  localStorage.setItem('scrollPosition', window.scrollY);
-});
-
-// On page load, restore scroll position if available
-if (localStorage.getItem('scrollPosition')) {
-  window.scrollTo(0, parseInt(localStorage.getItem('scrollPosition')));
-}
 
 export default router
